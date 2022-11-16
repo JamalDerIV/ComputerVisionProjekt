5f501a132d831ad555df1e6c6cf0f4e217489ca3
avaysert <a.vajsert12@yahoo.de>
2022-11-16 16:02:23 +0100

    refactored a method and got rid of unnecessary parameters

--- ComputerVisionProjekt/main.cpp
+++ ComputerVisionProjekt/main.cpp
@@ -8,6 +8,7 @@ class bgSub {
 private:
 	int imagesToLoad;
 	cv::Mat *images;
+	cv::Mat bgdImg;
 
 public:
 	bgSub(int pos) {
@@ -15,9 +16,7 @@ public:
 		images = new cv::Mat [pos];
 	}
 
-public:cv::Mat apply(int pos) {
-
-	
+public:void apply() {
 
 	//load all images into an array
 	for (int i = 1; i < imagesToLoad+1; i++) {
@@ -25,12 +24,12 @@ public:cv::Mat apply(int pos) {
 		char pos_str[7];
 		sprintf_s(pos_str, "%0.6d", i);
 
-		img_name << "data\\data_m2\\1\\input\\in" << pos_str << ".jpg";
+		img_name << "data\\data_m2\\4\\input\\in" << pos_str << ".jpg";
 		images[imagesToLoad-i] = cv::imread(img_name.str(), cv::IMREAD_GRAYSCALE);
 	}
 
-	//create a blank image
-	cv::Mat bgdImg(cv::Size(images[0].cols, images[0].rows), CV_8UC1);
+	//creates a blank image
+	bgdImg = cv::Mat::zeros(cv::Size(images[0].cols, images[0].rows), CV_8UC1);
 
 	//add the image grayscales and divide by images to load to get the average grayscale
 	for (int row = 0; row < images[0].rows; row++) {
@@ -46,58 +45,33 @@ public:cv::Mat apply(int pos) {
 			bgdImg.at<uchar>(row, col) = average;
 		}
 	}
-	
-	std::ostringstream new_img_name;
-	char pos_str[7];
-	sprintf_s(pos_str, "%0.6d", pos);
-	new_img_name << "data\\data_m2\\1\\input\\in" << pos_str << ".jpg";
-	cv::Mat imgNew = cv::imread(new_img_name.str(), cv::IMREAD_GRAYSCALE);
-
-	imshow("tei", imgNew);
-
-	for (int row = 0; row < imgNew.rows; row++) {
-		for (int col = 0; col < imgNew.cols; col++) {
-
-			imgNew.at<uchar>(row, col) = abs(imgNew.at<uchar>(row, col) - bgdImg.at<uchar>(row, col));
-
-			if (imgNew.at<uchar>(row, col) > 45) {
-				imgNew.at<uchar>(row, col) = 255;
-			}
-			else {
-				imgNew.at<uchar>(row, col) = 0;
-			}
-
-		}
-	}
-
-	imshow("testi", bgdImg);
-
-	/*cv::Mat graymat;
-	cv::cvtColor(solution, graymat, cv::COLOR_BGR2GRAY);
-
-
-	for (int row = 0; row < graymat.rows; row++) {
-		for (int col = 0; col < graymat.cols; col++) {
-			cv::Vec3b ignore = { 20,20,20 };
-			cv::Vec3b whitePixel = { 255,255,255 };
-			cv::Vec3b blackPixel = { 0,0,0 };
-
-			cv::Vec3b color = graymat.at<uchar>(row, col);
-
-			//std::cout << std::to_string(color[0]> ignore[0]);
-			//std::cout << std::to_string(graymat.at<cv::Vec3b>(row, col)[2]);
-
-			if (ignore[0] > graymat.at<uchar>(row, col)) {
-				graymat.at<uchar>(row, col) = 0;
-			}
-			else {
-				graymat.at<uchar>(row, col) = 255;
-			}
-		}
-	}*/
-
-	return imgNew;
 }
+	  cv::Mat substraction(int pos) {
+
+		  std::ostringstream new_img_name;
+		  char pos_str[7];
+		  sprintf_s(pos_str, "%0.6d", pos);
+		  new_img_name << "data\\data_m2\\4\\input\\in" << pos_str << ".jpg";
+		  cv::Mat imgNew = cv::imread(new_img_name.str(), cv::IMREAD_GRAYSCALE);
+
+		  for (int row = 0; row < imgNew.rows; row++) {
+			  for (int col = 0; col < imgNew.cols; col++) {
+
+				  imgNew.at<uchar>(row, col) = abs(imgNew.at<uchar>(row, col) - bgdImg.at<uchar>(row, col));
+
+				  //paint everything white that has a grayscale over 45 and black everything else
+				  if (imgNew.at<uchar>(row, col) > 45) {
+					  imgNew.at<uchar>(row, col) = 255;
+				  }
+				  else {
+					  imgNew.at<uchar>(row, col) = 0;
+				  }
+
+			  }
+		  }
+
+		  return imgNew;
+	  }
 };
 
 
@@ -105,11 +79,14 @@ public:cv::Mat apply(int pos) {
 
 
 int main() {
+
+	bgSub image(50);
+
+	image.apply();
+
 	for (int pos = 60; pos <= 1200; pos += 20) { // Stepping 10 Frames at a time might conflict with Background Subtraction
 
-		bgSub image(50);
-		
-		cv::Mat in_img = image.apply(pos);
+		cv::Mat in_img = image.substraction(pos);
 
 		imshow("testing", in_img);
 
