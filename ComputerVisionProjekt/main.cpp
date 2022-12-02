088f8eb574232d9132b9d767c24a47d39f46eca0
avaysert <a.vajsert12@yahoo.de>
2022-12-02 12:47:13 +0100

    added an parameter

--- ComputerVisionProjekt/main.cpp
+++ ComputerVisionProjekt/main.cpp
@@ -6,9 +6,12 @@ using namespace cv;
 
 int main() {
 
-	cv::Ptr<BackgroundSubtractor> mog2BS = createBackgroundSubtractorMOG2();
+	cv::Ptr<BackgroundSubtractorMOG2> mog2BS = createBackgroundSubtractorMOG2();
+	
+	mog2BS->setShadowThreshold(0.82);
 
-	for (int pos = 19; pos <= 1050; pos += 10) {
+
+	for (int pos = 19; pos <= 1050; pos += 1) {
 		std::ostringstream in_img_name, gt_img_name;
 		char pos_str[7]; 
 		sprintf_s(pos_str, "%0.6d", pos);
@@ -25,11 +28,12 @@ int main() {
 		cv::Mat mog2Mask;
 		mog2BS->apply(in_img, mog2Mask);
 
-		//imshow("Double Substraction",  in_img - (in_img - gt_img));
 		imshow("Mog2 Background Substraction",  mog2Mask);
 
+		std::cout << pos << std::endl;
+
 		// do 10 steps before waiting again 
-		if (pos % 1 == 0) {
+		if (pos % 10 == 0) {
 			int wait = cv::waitKey(0);
 			if (wait == 27) {
 				break; // ESC Key
