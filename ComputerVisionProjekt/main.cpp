ce437e35f934b2da705ec1d1b1e83125f82e4793
René Braun <rene.braun@fh-bielefeld.de>
2023-01-03 17:55:47 +0100

    reading detfile  per frame

--- ComputerVisionProjekt/main.cpp
+++ ComputerVisionProjekt/main.cpp
@@ -9,6 +9,22 @@ using namespace cv;
 
 class Detections {
 public:
+	float left, top, width, height, visibility, x, y, z;
+
+	void setData(float l, float t, float w, float h, float v, float xC, float yC, float zC) {
+		left = l;
+		top = t;
+		width = w;
+		height = h;
+		visibility = v;
+		x = xC;
+		y = yC;
+		z = zC;
+	}
+};
+
+class GroundTruth {
+public:
 	float left, top, width, height, x, y, z;
 
 	void setData(float l, float t, float w, float h, float xC, float yC, float zC) {
@@ -25,11 +41,16 @@ public:
 
 int main() {
 	int seqLength = GetPrivateProfileIntA("Sequence", "seqLength", 1050, "data\\data_m4\\1\\seqinfo.ini");
-	Detections (*gt)[200] = new Detections[seqLength][200];
+	GroundTruth (*gt)[200] = new GroundTruth[seqLength][200];
+	Detections *det = new Detections[50];
+
+	std::ifstream detfile("data\\data_m4\\1\\det\\det.txt");
+	int frame, nDetections = 0;
+	if (detfile >> frame);
 
 	// Reading (custom) gt file
 	//  -> the ',' seperator has been replaced with a ' ' whitespace
-	/*std::string line;
+	std::string line;
 	std::ifstream gtfile("data\\data_m4\\1\\gt\\gt.txt");
 
 	while (std::getline(gtfile, line)) {
@@ -44,9 +65,8 @@ int main() {
 	if (!gtfile.is_open()) {
 		std::cout << "Could not open Ground Truth file. Evaluation will be skipped." << std::endl;
 	}
-	*/
 
-	/*for (int pos = 1; pos <= seqLength; pos += 1) {
+	for (int pos = 1; pos <= seqLength; pos += 1) {
 		std::ostringstream in_img_name;
 		char pos_str[7]; 
 		sprintf_s(pos_str, "%0.6d", pos);
@@ -60,7 +80,28 @@ int main() {
 			return 1;
 		}
 
-		imshow("Image", in_img);
+		std::cout << "\nFrame: " << pos << std::endl;
+		// Reading detected values
+		nDetections = 0;
+		do {
+			float id, left, top, width, height, visibility, x, y, z;
+			detfile >> id >> left >> top >> width >> height >> visibility >> x >> y >> z;
+			det[nDetections].setData(left, top, width, height, visibility, x, y, z);
+			nDetections++;
+
+			std::cout << left << " - " << top << std::endl;
+
+			if (detfile) {
+				detfile >> frame;
+			}
+			else {
+				frame = 0;
+			}
+			
+		} while (frame == pos);
+		std::cout << "nDet " << nDetections << std::endl;
+
+		//imshow("Image", in_img);
 
 		std::cout << pos << std::endl;
 
@@ -73,7 +114,7 @@ int main() {
 		}
 
 		cv::destroyAllWindows();
-	}*/
+	}
 
 	return 0;
 }
