9ce23a2f5195ad29af99bb0b525ee958bc7cc58c
avaysert <a.vajsert12@yahoo.de>
2023-01-03 17:15:06 +0100

    added gt file import to heap memory

--- ComputerVisionProjekt/main.cpp
+++ ComputerVisionProjekt/main.cpp
@@ -1,13 +1,62 @@
 #include <opencv2\opencv.hpp>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
 
 using namespace cv;
 
+class Detections {
+public:
+	float left, top, width, height, x, y, z;
+
+	void setData(float l, float t, float w, float h, float xC, float yC, float zC) {
+		left = l;
+		top = t;
+		width = w;
+		height = h;
+		x = xC;
+		y = yC;
+		z = zC;
+	}
+};
+
 
 int main() {
+	Detections (*gt)[200] = new Detections[600][200];
+	
+
+	// Reading (custom) gt file
+		//  -> the ',' seperator has been replaced with a ' ' whitespace
+	std::string line;
+	std::ifstream gtfile("data\\data_m4\\1\\gt\\gt.txt");
+
+	while (std::getline(gtfile, line)) {
+		std::istringstream iss(line);
+		int frame, id, bb_left, bb_top, bb_width, bb_height, x, y, z;
+		if (!(iss >> frame >> id >> bb_left >> bb_top >> bb_width >> bb_height >> x >> y >> z)) {
+			break;
+		}
+		gt[frame][id].setData(bb_left, bb_top, bb_width, bb_height, x, y, z);
+
+		if (frame == 3 && id == 2) {
+			std::cout << gt[frame][id].left << " " << gt[frame][id].top << " " << gt[frame][id].width << " " << gt[frame][id].height << std::endl;
+		}
+		if (frame == 2 && id == 3) {
+			std::cout << gt[frame][id].left << " " << gt[frame][id].top << " " << gt[frame][id].width << " " << gt[frame][id].height << std::endl;
+		}
+
+	}
+	
+
+	if (!gtfile.is_open()) {
+		std::cout << "Could not open Ground Truth file. Evaluation will be skipped." << std::endl;
+	}
+	
+	//std::cout << sizeof(gt) << std::endl;
 
 
-	for (int pos = 1; pos <= 1050; pos += 1) {
+	/*for (int pos = 1; pos <= 1050; pos += 1) {
 		std::ostringstream in_img_name;
 		char pos_str[7]; 
 		sprintf_s(pos_str, "%0.6d", pos);
@@ -21,13 +70,12 @@ int main() {
 			return 1;
 		}
 
-
 		imshow("Image", in_img);
 
 		std::cout << pos << std::endl;
 
 		// do 10 steps before waiting again 
-		if (pos % 10 == 0) {
+		if (pos % 1 == 0) {
 			int wait = cv::waitKey(0);
 			if (wait == 27) {
 				break; // ESC Key
@@ -35,6 +83,7 @@ int main() {
 		}
 
 		cv::destroyAllWindows();
-	}
+	}*/
+
 	return 0;
 }
