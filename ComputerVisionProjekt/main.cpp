e29082e419beb69e03fdd285eb279ceb14f0bb76
avaysert <a.vajsert12@yahoo.de>
2023-01-10 17:51:36 +0100

    added a raw hungarian method

--- ComputerVisionProjekt/main.cpp
+++ ComputerVisionProjekt/main.cpp
@@ -58,10 +58,11 @@ public:
 class TrackedObject {
 public:
 	Detections det;
-	int id;
-	TrackedObject(Detections d, int i) {
+	int id, cutsRect;
+	TrackedObject(Detections d, int i,int c) {
 		det = d;
 		id = i;
+		cutsRect = c;
 	}
 	int getX() {
 		return int(det.left);
@@ -86,6 +87,68 @@ double iou(Rect boxGT, Rect boxPredicted) {
 
 	return (double)iArea / (double)(areaA + areaB - iArea);
 };
+
+void hungarian(Mat values, int tries) {
+	tries++;
+	std::cout << values << std::endl;
+
+	for (int i = 0; i < values.rows; i++) {
+		int minNumber = 100;
+
+		for (int j = 0; j < values.cols; j++) {
+			//change 100 to the number that you want to subtract as a maximum, so only detections that are close will be subtracted
+			if (values.at<uchar>(i, j) < 100 && values.at<uchar>(i, j) < minNumber && values.at<uchar>(i, j) != 0) {
+				minNumber = values.at<uchar>(i, j);
+			}
+		}
+
+		for (int j = 0; j < values.cols; j++) {
+			//change 100 to the number that you want to subtract as a maximum, so only detections that are close will be subtracted
+			if (values.at<uchar>(i, j) < 100 && minNumber != 100) {
+				if (values.at<uchar>(i, j) == 0) {
+					continue;
+				}
+				values.at<uchar>(i, j) -= minNumber;
+			}
+		}
+	}
+
+	std::cout << values << std::endl;
+
+	bool everyRowZeros = true;
+	for (int i = 0; i < values.rows; i++) {
+		int countZeros=0;
+		for (int j = 0; j < values.cols; j++) {
+			if (values.at<uchar>(i, j) == 0) {
+				countZeros++;
+			}
+		}
+		std::cout << "Try: " << tries << " | " << i+1 << ". row zeros: " << countZeros << std::endl;
+		if (countZeros == 0) {
+			everyRowZeros = false;
+		}
+	}
+	
+
+	if (!everyRowZeros && tries < 3) {
+		hungarian(values, tries);
+	}
+}
+
+void calcMatrix(std::vector<TrackedObject> trackedObjects, std::vector<Detections> detections){
+	Mat values = cv::Mat::zeros(cv::Size( detections.size(), trackedObjects.size()), CV_8UC1);
+	int count=0;
+	for (int i = 0; i < trackedObjects.size(); i++) {
+		for (int j = 0; j < detections.size();j++) {
+			count++;
+			//std::cout << "i: " << i << " " << trackedObjects[i].det.getRect() << " | j: " << j << " " << detections[j].getRect() << std::endl;
+			//std::cout << count << " mal aufgerufen" << " tracked size: " << trackedObjects.size() << "det size: " << detections.size() << " | matrix size: " << values.size() << std::endl;
+			values.at<uchar>(i,j) = 100-(iou(trackedObjects[i].det.getRect(), detections[j].getRect())*100);
+		}
+	}
+	
+	hungarian(values,0);
+}
 
 int main() {
 	String filepath("data\\data_m4\\2\\");
@@ -155,29 +218,35 @@ int main() {
 		} while (frame == pos);
 
 		// push detections into trackedObjects
-		for (int i = 0; i < det.size(); i++) {
-			TrackedObject a(det[i], trackedObjects.size() + 1);
-			trackedObjects.push_back(a);
-			//cv::putText(in_img, std::to_string(a.id), { a.getX(), a.getY() + 20 }, cv::FONT_HERSHEY_SIMPLEX, 0.8, { 0,255,0 }, 2);
+		if (trackedObjects.size() <= 0) {
+			for (int i = 0; i < det.size(); i++) {
+				TrackedObject a(det[i], trackedObjects.size() + 1, 0);
+				trackedObjects.push_back(a);
+				//cv::putText(in_img, std::to_string(a.id), { a.getX(), a.getY() + 20 }, cv::FONT_HERSHEY_SIMPLEX, 0.8, { 0,255,0 }, 2);
+			}
 		}
 
 		//draw tracked Objects
 		for (int i = 0; i < trackedObjects.size(); i++) {
 			rectangle(in_img, trackedObjects[i].det.getRect(), Scalar(0, 255, 0), 1);
 			cv::putText(in_img, std::to_string(trackedObjects[i].id), { trackedObjects[i].getX(), trackedObjects[i].getY() + 20 }, cv::FONT_HERSHEY_SIMPLEX, 0.8, { 0,255,0 }, 2);
-			std::cout << trackedObjects[i].det.getRect() << " | id: " << trackedObjects[i].id << std::endl;
+			//std::cout << trackedObjects[i].det.getRect() << " | id: " << trackedObjects[i].id << std::endl;
 			
 		}
+		
+		if (pos >= 2) {
+			calcMatrix(trackedObjects, det); 
+		}
 
-		trackedObjects.clear();
+		//trackedObjects.clear();
 
 		//draw groundtruths
 		/*
 		for (int i = 1; i < nGroundtruths; i++) {
 			rectangle(in_img, Rect(gt[i].left, gt[i].top, gt[i].width, gt[i].height), Scalar(255, 0, 0), 1);
 			//std::cout << gt[i].left << " - " << gt[i].top << " - " << gt[i].width << " - " << gt[i].height << " - " << std::endl;
-		}
-		*/
+		}*/
+		
 
 		imshow("Image", in_img);
 
